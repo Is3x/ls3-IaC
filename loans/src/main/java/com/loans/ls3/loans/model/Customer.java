3a4835fc237b368c150ef6919225aae33ca1a055
Is3x <85684507+Is3x@users.noreply.github.com>
2022-02-09 05:04:49 +0300

    create customer and loans model

---
+++ loans/src/main/java/com/loans/ls3/loans/model/Customer.java
@@ -0,0 +1,13 @@
+package com.loans.ls3.loans.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter @Setter @ToString
+public class Customer {
+
+    private int customerId;
+
+}
