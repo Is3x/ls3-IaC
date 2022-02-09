71eb436bbb011c4269673f99722b51e5097fbf6b
Is3x <85684507+Is3x@users.noreply.github.com>
2022-02-09 22:12:47 +0300

    update loans service

--- accounts/src/main/java/com/accounts/ls3/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/accounts/ls3/accounts/controller/AccountsController.java
@@ -1,19 +1,30 @@
 package com.accounts.ls3.accounts.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.accounts.ls3.accounts.config.AccountsServiceConfig;
 import com.accounts.ls3.accounts.model.Accounts;
 import com.accounts.ls3.accounts.model.Customer;
+import com.accounts.ls3.accounts.model.Properties;
 import com.accounts.ls3.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 
 @RestController
 public class AccountsController {
 
     @Autowired
     private AccountsRepository accountsRepository;
+
+    @Autowired
+    AccountsServiceConfig accountsConfig;
 
     @PostMapping("/myAccount")
     public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -25,6 +36,15 @@ public class AccountsController {
             return null;
         }
 
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
