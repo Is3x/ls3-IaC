18cab3f5400fcae479f4226482bbeb3c65a9ff90
Is3x <85684507+Is3x@users.noreply.github.com>
2022-02-09 22:11:32 +0300

    updating cards microservice

--- cards/src/main/java/com/cards/ls3/cards/controller/CardsController.java
+++ cards/src/main/java/com/cards/ls3/cards/controller/CardsController.java
@@ -3,13 +3,19 @@ package com.cards.ls3.cards.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cards.ls3.cards.config.CardsServiceConfig;
 import com.cards.ls3.cards.model.Cards;
 import com.cards.ls3.cards.model.Customer;
+import com.cards.ls3.cards.model.Properties;
 import com.cards.ls3.cards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 
 @RestController
@@ -17,6 +23,9 @@ public class CardsController {
 
     @Autowired
     private CardsRepository cardsRepository;
+
+    @Autowired
+    CardsServiceConfig cardsConfig;
 
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer) {
@@ -27,6 +36,15 @@ public class CardsController {
             return null;
         }
 
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
